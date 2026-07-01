3818d383661a965d78676b20f40907a38e6be0bc
AlanRamirezDev <alan_ramirez_juarez@hotmail.com>
2026-07-01 13:55:31 -0600

    feat: implementacion de versionado (v1)

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,17 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReportController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-Route::post('/reportes/generar', [ReportController::class, 'generate']);
-
 // Endpoint Activar Servidor
 Route::get('/ping', function () {
     return response()->json(['status' => 'online', 'message' => 'Servidor Activo']);
+});
+
+Route::prefix('v1')->group(function () {
+    
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+
+    Route::post('/reportes/generar', [ReportController::class, 'generate']);
+
 });
